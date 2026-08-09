b1a5977cab650673df4ac1af2546489d78c0f011
Arun Kumar <arun987604@gmail.com>
2026-08-09 16:34:31 +0530

    chore: reformat argocd_allowed_cidrs variable assignment in dev.tfvars

--- infrastructure/app-cluster/outputs.tf
+++ infrastructure/app-cluster/outputs.tf
@@ -1,24 +1,11 @@
 output "vpc_id" {
-  description = "VPC ID"
-  value       = module.networking.vpc_id
+  value = module.networking.vpc_id
 }
 
 output "public_subnet_ids" {
-  description = "Public Subnet IDs"
-  value       = module.networking.public_subnet_ids
+  value = module.networking.public_subnet_ids
 }
 
 output "private_app_subnet_ids" {
-  description = "Private Subnet IDs"
-  value       = module.networking.private_app_subnet_ids
-}
-
-output "eks_cluster_name" {
-  description = "EKS Cluster Name"
-  value       = module.eks.cluster_name
-}
-
-output "eks_cluster_endpoint" {
-  description = "EKS Cluster API Endpoint"
-  value       = module.eks.cluster_endpoint
+  value = module.networking.private_app_subnet_ids
 }
