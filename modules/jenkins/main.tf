6b9d11386d0f1c98d9aa85178dbde5e673c17de1
Arun Kumar <arun987604@gmail.com>
2026-08-09 15:24:31 +0530

    fix: attach DynamoDB and Admin policies to Jenkins IAM role for state locking and infrastructure provisioning

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -22,10 +22,21 @@ resource "aws_iam_role_policy_attachment" "ecr" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
 
-resource "aws_iam_role_policy_attachment" "terraform_state" {
+resource "aws_iam_role_policy_attachment" "terraform_s3" {
   role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+resource "aws_iam_role_policy_attachment" "dynamodb" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "administrator" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
 
 resource "aws_iam_instance_profile" "this" {
   name = "${var.name}-${var.environment}-jenkins-profile"
