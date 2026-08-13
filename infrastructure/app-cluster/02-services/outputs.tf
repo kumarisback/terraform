a05fd491d923367f36738cfb94234813c001aec9
Arun Kumar <arun987604@gmail.com>
2026-08-13 14:17:16 +0530

    output argourl

--- infrastructure/app-cluster/02-services/outputs.tf
+++ infrastructure/app-cluster/02-services/outputs.tf
@@ -2,17 +2,23 @@ output "argocd_namespace" {
   value = helm_release.argocd.namespace
 }
 
-output "argocd_server_url" {
-  description = "The external URL/address for the ArgoCD server"
-  value       = var.enable_argocd_loadbalancer ? "https://${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname}" : "ClusterIP (Use kubectl port-forward)"
-}
-
-# Data source to fetch live status of the Service created by Helm
-data "kubernetes_service" "argocd_server" {
+data "kubernetes_service_v1" "argocd_server" {
   depends_on = [helm_release.argocd]
 
   metadata {
     name      = "argocd-server"
     namespace = "argocd"
   }
+}
+
+output "argocd_server_url" {
+  description = "The external URL/address for the ArgoCD server"
+  value = var.enable_argocd_loadbalancer ? (
+    length(data.kubernetes_service_v1.argocd_server.status[0].load_balancer[0].ingress) > 0 ? (
+      try(
+        "http://${data.kubernetes_service_v1.argocd_server.status[0].load_balancer[0].ingress[0].hostname}",
+        "http://${data.kubernetes_service_v1.argocd_server.status[0].load_balancer[0].ingress[0].ip}"
+      )
+    ) : "Pending LoadBalancer Provisioning..."
+  ) : "ClusterIP (Use kubectl port-forward)"
 }
--- infrastructure/app-cluster/02-services/providers.tf
+++ infrastructure/app-cluster/02-services/providers.tf
@@ -45,3 +45,15 @@ provider "helm" {
     }
   }
 }
+
+# Configure Kubernetes provider using EKS cluster outputs
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.infra.outputs.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.infra.outputs.cluster_name]
+  }
+}
