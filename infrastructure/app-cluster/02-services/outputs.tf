1f8ab7e8e05bd544cfe863fb1799aaee493fb0ae
Arun Kumar <arun987604@gmail.com>
2026-08-13 14:13:46 +0530

    output argo url

--- infrastructure/app-cluster/02-services/outputs.tf
+++ infrastructure/app-cluster/02-services/outputs.tf
@@ -1,3 +1,18 @@
 output "argocd_namespace" {
   value = helm_release.argocd.namespace
 }
+
+output "argocd_server_url" {
+  description = "The external URL/address for the ArgoCD server"
+  value       = var.enable_argocd_loadbalancer ? "https://${data.kubernetes_service.argocd_server.status[0].load_balancer[0].ingress[0].hostname}" : "ClusterIP (Use kubectl port-forward)"
+}
+
+# Data source to fetch live status of the Service created by Helm
+data "kubernetes_service" "argocd_server" {
+  depends_on = [helm_release.argocd]
+
+  metadata {
+    name      = "argocd-server"
+    namespace = "argocd"
+  }
+}
