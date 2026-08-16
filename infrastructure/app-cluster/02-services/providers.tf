4343d8b1147984e88665daffe37a928cdc4c23d3
Arun Kumar <arun987604@gmail.com>
2026-08-16 16:15:02 +0530

    chore: update helm provider configuration syntax to HCL block format

--- infrastructure/app-cluster/02-services/providers.tf
+++ infrastructure/app-cluster/02-services/providers.tf
@@ -34,14 +34,14 @@ data "terraform_remote_state" "infra" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = data.terraform_remote_state.infra.outputs.cluster_endpoint
     cluster_ca_certificate = base64decode(data.terraform_remote_state.infra.outputs.cluster_certificate_authority_data)
 
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.infra.outputs.cluster_name]
       command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.infra.outputs.cluster_name]
     }
   }
 }
